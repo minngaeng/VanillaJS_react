591fcbc149c4899bb3ed2df418a3853d7d0e2279
minngaeng <minngaeng@gmail.com>
2024-11-23 00:14:06 +0900

    chore: babel script command setting

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,18 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = App;
 var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
+var _jsxRuntime = require("react/jsx-runtime");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var App = function App() {
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
-};
-var root = _client["default"].createRoot(document.getElementById("app"));
-root.render(/*#__PURE__*/_react["default"].createElement(App, null));
+function App() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("h1", {
+      children: "Hello World"
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+      children: " how to make a vdom"
+    })]
+  });
+}
